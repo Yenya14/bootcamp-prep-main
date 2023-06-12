85b58463de006109de23d46fdcc0619728852748
Yenya14 <leeza.mhr@gmail.com>
2023-06-12 11:48:58 +0545

    commiting for array i

--- bootcamp-prep-main/workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ bootcamp-prep-main/workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function myUnshift(oArr, value) {
+    let newArray = [value];
+    for (let i = 0; i < oArr.length; i++) {
+      let value = oArr[i];
+      newArray.push(value);
+    }
+    return newArray;
+}
