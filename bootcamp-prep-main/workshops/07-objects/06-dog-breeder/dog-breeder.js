93570f31430383caa05aacfe2a7a02e8c412aa99
Yenya14 <leeza.mhr@gmail.com>
2023-07-07 11:09:37 +0545

    commiting for remaining object

--- bootcamp-prep-main/workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ bootcamp-prep-main/workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function dogBreeder(name='Steve', age=0) {
+    if (typeof name === 'number') {
+      age = name;
+      name = 'Steve'
+    }
+
+    let newDog =  {
+      name: name,
+      age: age
+    };
+    
+    return newDog;
+  }
--- bootcamp-prep-main/workshops/07-objects/07-attendance-check/attendance-check.js
+++ bootcamp-prep-main/workshops/07-objects/07-attendance-check/attendance-check.js
@@ -38,3 +38,30 @@ let classRoom = [
 ];
 
 // YOUR CODE BELOW
+// function attendanceCheck(day) {
+//     switch (day) {
+//       case 'Monday':
+//       case 'Tuesday':
+//       case 'Thursday':
+//         return ['Marnie', 'Shoshanna'];
+//       case 'Wednesday':
+//         return ['Marnie', 'Lena'];
+//       case 'Friday':
+//         return ['Marnie', 'Lena', 'Jessa'];
+//       default:
+//         return [];
+//     }
+//   }
+  
+function attendanceCheck(day) {
+    let attendance = {
+      Monday: ['Marnie', 'Shoshanna'],
+      Tuesday: ['Marnie', 'Shoshanna'],
+      Wednesday: ['Marnie', 'Lena'],
+      Thursday: ['Marnie', 'Shoshanna'],
+      Friday: ['Marnie', 'Lena', 'Jessa'],
+    };
+  
+    return attendance[day] || [];
+  }
+  
